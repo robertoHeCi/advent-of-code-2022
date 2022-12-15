5f7e43a7f60fe7122a348f9b49b5fa6d0dd4af73
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2022-12-15 01:28:55 +0100

    feat: add solutions template

---
+++ template.js
@@ -0,0 +1,17 @@
+// Personal data set
+const inputCase = ``
+
+// Provided test case
+const testCase = ``;
+
+// Solution
+const resolve = (input) => {
+}
+
+const test = resolve(testCase);
+
+console.assert(test === 'PUT HERE TEST SOLUTION', 'Test case failed');
+
+const output = resolve(inputCase);
+
+console.log(`The solution is ${output}`);
